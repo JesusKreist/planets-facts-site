4ed0575a5b215c16bd66aac809361bdbb5ddfa2b
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2023-01-08 21:22:05 +0100

    Add initial commit with theme file configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "@fontsource/spartan/100.css";
+import "@fontsource/spartan/200.css";
+import "@fontsource/spartan/300.css";
+import "@fontsource/spartan/400.css";
+import "@fontsource/spartan/500.css";
+import "@fontsource/spartan/600.css";
+import "@fontsource/spartan/700.css";
+import "@fontsource/spartan/800.css";
+import "@fontsource/spartan/900.css";
+import "@fontsource/antonio/100.css";
+import "@fontsource/antonio/200.css";
+import "@fontsource/antonio/300.css";
+import "@fontsource/antonio/400.css";
+import "@fontsource/antonio/500.css";
+import "@fontsource/antonio/600.css";
+import "@fontsource/antonio/700.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme} resetCSS>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
---
+++ styles/theme.ts
@@ -0,0 +1,47 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const breakpoints = {
+  sm: "320px",
+  md: "768px",
+  lg: "960px",
+  xl: "1200px",
+  "2xl": "1440px",
+};
+
+const theme = extendTheme({
+  textStyles: {
+    h1: {
+      fontSize: ["80px"],
+      fontWeight: "medium",
+      fontFamily: "Antonio",
+      lineHeight: "103px",
+    },
+    h2: {
+      fontSize: ["40px"],
+      fontWeight: "medium",
+      fontFamily: "Antonio",
+      lineHeight: "52px",
+    },
+    h3: {
+      fontSize: ["12px"],
+      fontWeight: "bold",
+      fontFamily: "Spartan",
+      lineHeight: "25px",
+    },
+    h4: {
+      fontSize: ["11px"],
+      fontWeight: "bold",
+      fontFamily: "Spartan",
+      lineHeight: "25px",
+    },
+    p: {
+      fontSize: ["14px"],
+      fontWeight: "regular",
+      fontFamily: "Spartan",
+      lineHeight: "25px",
+    },
+  },
+  breakpoints,
+});
+
+export default theme;
