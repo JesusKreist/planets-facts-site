17d2c4c84751cb3379fb2ae6cf3535f9ae5de62e
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2023-01-12 18:42:52 +0100

    Move planet info buttons to separate component.

--- src/components/PlanetPage/PlanetInfoButtons.tsx
+++ src/components/PlanetPage/PlanetInfoButtons.tsx
@@ -1,58 +1,96 @@
 import { Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
-const PlanetInfoButtons = () => {
+interface PlanetInfoButtonProps {
+  planetColour: string;
+  isActive: boolean;
+  infoNumber: number;
+  infoTitle: string;
+}
+
+const PlanetInfoButton: React.FC<PlanetInfoButtonProps> = ({
+  planetColour,
+  isActive,
+  infoNumber,
+  infoTitle,
+}) => {
+  return (
+    <Flex
+      bgColor={`${isActive ? planetColour : "transparent"}`}
+      paddingBlock="0.3rem"
+      paddingLeft="2rem"
+      gap="1rem"
+      width="100%"
+      height="48px"
+      alignItems="center"
+      borderWidth={isActive ? "0px" : "2px"}
+      borderColor={isActive ? "transparent" : "rgb(151, 151, 151, 0.2)"}
+      as="button"
+      _hover={{
+        bgColor: `${isActive ? planetColour : "rgb(216, 216, 216, 0.2)"}`,
+        borderWidth: `0px`,
+      }}
+    >
+      <Text
+        fontFamily="spartan"
+        fontWeight="bold"
+        fontSize="0.75rem"
+        lineHeight="25px"
+        letterSpacing="2.57px"
+        width="1.58rem"
+        opacity={0.5}
+      >
+        0{infoNumber}
+      </Text>
+      <Text
+        fontFamily="spartan"
+        fontWeight="bold"
+        fontSize="0.75rem"
+        lineHeight="25px"
+        letterSpacing="2.57px"
+      >
+        {infoTitle}
+      </Text>
+    </Flex>
+  );
+};
+
+interface PlanetInfoSwitcherProps {
+  planetName: string;
+  activePage: string;
+}
+
+const PlanetInfoSwitcher = () => {
+  const planetName = "mercury";
+  const planetColour = `planets.${planetName}`;
+
   return (
     <Flex
       // border="2px solid blue"
       direction="column"
       justifyContent="space-between"
     >
-      <Flex
-        bgColor={`planets.${"mercury".toLocaleLowerCase()}`}
-        paddingBlock="0.3rem"
-        paddingLeft="2rem"
-        gap="1rem"
-        width="100%"
-        height="48px"
-        alignItems="center"
-      >
-        <Text
-          fontFamily="spartan"
-          fontWeight="bold"
-          fontSize="0.75rem"
-          lineHeight="25px"
-          letterSpacing="2.57px"
-          opacity={0.5}
-        >
-          01
-        </Text>
-        <Text
-          fontFamily="spartan"
-          fontWeight="bold"
-          fontSize="0.75rem"
-          lineHeight="25px"
-          letterSpacing="2.57px"
-        >
-          OVERVIEW
-        </Text>
-      </Flex>
-      <Flex
-        border="1px solid white"
-        paddingBlock="0.3rem"
-        width="100%"
-        height="48px"
-        alignItems="center"
-      ></Flex>
-      <Flex
-        border="1px solid white"
-        paddingBlock="0.3rem"
-        width="100%"
-        height="48px"
-        alignItems="center"
-      ></Flex>
+      <PlanetInfoButton
+        infoNumber={1}
+        infoTitle="OVERVIEW"
+        // infoTitle="INTERNAL STRUCTURE"
+        isActive={true}
+        planetColour={planetColour}
+      />
+      <PlanetInfoButton
+        infoNumber={2}
+        infoTitle="INTERNAL STRUCTURE"
+        isActive={false}
+        planetColour={planetColour}
+      />
+      <PlanetInfoButton
+        infoNumber={3}
+        infoTitle="SURFACE GEOLOGY"
+        isActive={false}
+        planetColour={planetColour}
+      />
     </Flex>
   );
 };
 
-export default PlanetInfoButtons;
+export default PlanetInfoSwitcher;
