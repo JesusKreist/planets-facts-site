6b0bac9e1e62b35ecca3c6df715a97a766201b1a
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2023-01-14 19:19:28 +0100

    Add display of dynamic planet info text

--- src/components/PlanetPage/PlanetInfoText.tsx
+++ src/components/PlanetPage/PlanetInfoText.tsx
@@ -3,11 +3,51 @@ import { usePlanetsStore } from "../../store/planetsStore";
 
 const PlanetInfoText = () => {
   const currentPlanet = usePlanetsStore((state) => state.currentPlanet);
+  const planetInfo = usePlanetsStore((state) => state.planetInfo);
+
+  const planetInfoText = `${currentPlanet[planetInfo].content}`;
+
+  // console.log("currentPlanet:>> ", currentPlanet);
+  // console.log("planetInfo:>> ", planetInfo);
+  // console.log(planetInfoText);
+  // // const
+
   return (
-    <Flex border="2px solid red">
-      <Text>MERCURY</Text>
-      <Text></Text>
-      <Text></Text>
+    <Flex direction="column" justifyContent="space-between">
+      <Text
+        fontSize="5rem"
+        fontFamily="Antonio"
+        fontWeight="medium"
+        height="31.9%"
+      >
+        {currentPlanet.name}
+      </Text>
+      <Text
+        fontSize="0.875rem"
+        fontFamily="Spartan"
+        fontWeight="normal"
+        lineHeight="1.5625rem"
+      >
+        {planetInfoText}
+      </Text>
+      <Box>
+        <Text
+          as="span"
+          fontSize="0.875rem"
+          fontFamily="Spartan"
+          fontWeight="regular"
+        >
+          Source:{" "}
+        </Text>
+        <Text
+          as="span"
+          fontSize="0.875rem"
+          fontFamily="Spartan"
+          fontWeight="bold"
+        >
+          Wikipedia
+        </Text>
+      </Box>
     </Flex>
   );
 };
