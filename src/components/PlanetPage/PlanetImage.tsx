d160a3a790d999eaefa6ba1ab4b6950779c13b2d
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2023-01-16 20:37:55 +0100

    Add dynamic display of images depending on which text is being displayed

--- src/components/PlanetPage/PlanetImage.tsx
+++ src/components/PlanetPage/PlanetImage.tsx
@@ -1,25 +1,63 @@
 import { Box, Flex } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { usePlanetsStore } from "../../store/planetsStore";
 
 const PlanetImage = () => {
+  const planetName = usePlanetsStore((state) =>
+    state.currentPlanet.name.toLowerCase()
+  );
+  const planetInfo = usePlanetsStore((state) => state.planetInfo);
+  const [currentImage, setCurrentImage] = useState(
+    `/assets/planet-${planetName}.svg`
+  );
+
+  useEffect(() => {
+    switch (planetInfo) {
+      case "overview":
+        setCurrentImage(`/assets/planet-${planetName}.svg`);
+        break;
+      case "structure":
+        setCurrentImage(`/assets/planet-${planetName}-internal.svg`);
+        break;
+      case "geology":
+        setCurrentImage(`/assets/planet-${planetName}.svg`);
+        break;
+      default:
+        break;
+    }
+  }, [planetInfo, planetName, setCurrentImage]);
+
   return (
     <Flex
       alignItems="center"
-      border="2px solid white"
+      // border="2px solid white"
       gridColumn="9 / span 8"
       gridRow="5 / span 9"
-
+      position="relative"
       // gridRow="7 / span 9"
     >
+      <Box
+        className="planet-image__geology"
+        // border="1px solid red"
+        width="56.2%"
+        height="58.62%"
+        top="75%"
+        left="22.2%"
+        position="absolute"
+        bgImage={`/assets/geology-${planetName}.png`}
+        bgSize="contain"
+        bgRepeat="no-repeat"
+        bgPosition="center"
+        display={planetInfo === "geology" ? "block" : "none"}
+      ></Box>
       <Box
         width="100%"
         height="0"
         paddingBottom="100%"
-        bgImage="/assets/planet-mercury.svg"
+        bgImage={currentImage}
         bgSize="cover"
-        // height="290px"
         rounded="full"
-        border="1px solid red"
+        // border="1px solid red"
       ></Box>
     </Flex>
   );
