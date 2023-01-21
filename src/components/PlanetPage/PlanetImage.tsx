c5770cf54d3a3e100923f166f0b7f8649f9cb57f
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2023-01-21 15:02:26 +0100

    Add smooth transition of planet images on planet info change

--- src/components/PlanetPage/PlanetImage.tsx
+++ src/components/PlanetPage/PlanetImage.tsx
@@ -1,5 +1,5 @@
 import { Box, Flex, Image } from "@chakra-ui/react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import { heartBeat, slideInCard } from "../../../animation/animationVariants";
 import { usePlanetsStore } from "../../store/planetsStore";
@@ -64,93 +64,100 @@ const PlanetImage = () => {
   }, [planetInfo, planetName, setCurrentImage]);
 
   return (
-    <Flex
-      key={`${planetName}-${planetInfo}-image`}
-      as={motion.div}
-      variants={slideInCard}
-      initial="offscreen"
-      animate="onscreen"
-      viewport={{ once: true, amount: 0.8 }}
-      exit="exit"
-      alignItems="center"
-      justifyContent="center"
-      // border="2px solid white"
-      gridColumn={{ base: "1 / -1", md: "3 / -3", lg: "3 / 21", xl: "5 / 20" }}
-      gridRow={{ lg: "3 / 18" }}
-      position="relative"
-    >
-      <Box
-        key={`${planetName}-${planetInfo}-image-geology`}
+    <AnimatePresence exitBeforeEnter>
+      <Flex
+        key={`${planetName}-${planetInfo}-image`}
         as={motion.div}
-        variants={heartBeat}
+        variants={slideInCard}
         initial="offscreen"
-        whileInView="onscreen"
+        animate="onscreen"
         viewport={{ once: true, amount: 0.8 }}
         exit="exit"
-        className="planet-image__geology"
-        width={{ base: "20%", md: "15%", lg: "25.2%" }}
-        height="58.62%"
-        top="50%"
-        left={{ md: "unset", lg: "37.6%" }}
-        position="absolute"
-        bgImage={`/assets/geology-${planetName}.png`}
-        bgSize="contain"
-        bgRepeat="no-repeat"
-        bgPosition="center"
-        display={planetInfo === "geology" ? "block" : "none"}
-      ></Box>
-
-      <Box
-        width="100%"
-        height="100%"
-        bgImage={currentImage}
-        bgSize={{
-          base: `${mobileHeights[planetName]}`,
-          md: `${tabletHeights[planetName]}`,
-          lg: planetIsSaturnOrJupiter ? "contain" : "auto",
-        }}
-        bgRepeat="no-repeat"
-        bgPosition="center"
-      ></Box>
-
-      {/* <Box
-        display={{ base: "none", lg: "block" }}
-        width="100%"
-        height="100%"
-        bgImage={currentImage}
-        bgSize={planetIsSaturnOrJupiter ? "contain" : "auto"}
-        // bgSize="contain"
-        bgRepeat="no-repeat"
-        bgPosition="center"
-        // border="3px solid red"
-      ></Box> */}
-
-      {/* <Flex
-        height="100%"
         alignItems="center"
-        display={{ base: "flex", lg: "none" }}
+        justifyContent="center"
+        // border="2px solid white"
+        gridColumn={{
+          base: "1 / -1",
+          md: "3 / -3",
+          lg: "3 / 21",
+          xl: "5 / 20",
+        }}
+        gridRow={{ lg: "3 / 18" }}
+        position="relative"
       >
-        <Image
-          src={currentImage}
-          alt="planet image"
-          overflow="hidden"
-          objectFit="contain"
-          maxHeight={`${tabletHeights[planetName]}`}
-        />
-      </Flex> */}
+        <Box
+          key={`${planetName}-${planetInfo}-image-geology`}
+          as={motion.div}
+          variants={heartBeat}
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: true, amount: 0.8 }}
+          exit="exit"
+          className="planet-image__geology"
+          width={{ base: "20%", md: "15%", lg: "25.2%" }}
+          height="58.62%"
+          top="50%"
+          left={{ md: "unset", lg: "37.6%" }}
+          position="absolute"
+          bgImage={`/assets/geology-${planetName}.png`}
+          bgSize="contain"
+          bgRepeat="no-repeat"
+          bgPosition="center"
+          display={planetInfo === "geology" ? "block" : "none"}
+        ></Box>
 
-      {/* <Box
-        width="100%"
-        // height="0"
-        paddingBottom="100%"
-        bgImage={currentImage}
-        bgSize={planetIsSaturnOrJupiter ? "contain" : "auto"}
-        // bgSize="contain"
-        bgRepeat="no-repeat"
-        bgPosition="center"
-        // border="3px solid red"
-      ></Box> */}
-    </Flex>
+        <Box
+          width="100%"
+          height="100%"
+          bgImage={currentImage}
+          bgSize={{
+            base: `${mobileHeights[planetName]}`,
+            md: `${tabletHeights[planetName]}`,
+            lg: planetIsSaturnOrJupiter ? "contain" : "auto",
+          }}
+          bgRepeat="no-repeat"
+          bgPosition="center"
+        ></Box>
+
+        {/* <Box
+      display={{ base: "none", lg: "block" }}
+      width="100%"
+      height="100%"
+      bgImage={currentImage}
+      bgSize={planetIsSaturnOrJupiter ? "contain" : "auto"}
+      // bgSize="contain"
+      bgRepeat="no-repeat"
+      bgPosition="center"
+      // border="3px solid red"
+    ></Box> */}
+
+        {/* <Flex
+      height="100%"
+      alignItems="center"
+      display={{ base: "flex", lg: "none" }}
+    >
+      <Image
+        src={currentImage}
+        alt="planet image"
+        overflow="hidden"
+        objectFit="contain"
+        maxHeight={`${tabletHeights[planetName]}`}
+      />
+    </Flex> */}
+
+        {/* <Box
+      width="100%"
+      // height="0"
+      paddingBottom="100%"
+      bgImage={currentImage}
+      bgSize={planetIsSaturnOrJupiter ? "contain" : "auto"}
+      // bgSize="contain"
+      bgRepeat="no-repeat"
+      bgPosition="center"
+      // border="3px solid red"
+    ></Box> */}
+      </Flex>
+    </AnimatePresence>
   );
 };
 
